0c43343def314b5266e0ece74db61b480b74a976
Jickx <63093696+Jickx@users.noreply.github.com>
2021-06-04 19:35:00 +0300

    Google Fit Weigh-ins data into csv

--- fit.py
+++ fit.py
@@ -1,17 +1,35 @@
-import json
 import requests
+import datetime
+import csv
 
-api_url = "https://www.googleapis.com/fitness/v1/users/me/dataSources/raw:com.google.weight:com.picooc.international:/datasets/0-1622730223497000000"
+api_url = "https://www.googleapis.com/fitness/v1/users/me/dataSources/derived:com.google.weight:com.google.android.gms:merge_weight/datasets/0-1622803690000000000"
 
-access_token = "ya29.a0AfH6SMDl51guxlitl6JrQXqthtUphUJ3BqnIlKaBT-RGtGxR4C-TQ8zn94qSAf0tKxzcAw1eiroII9TqvkyNYrPSyT79r5e90o_n4vqmSwlXW7kS-mV7fnV1ezvab8bR3btQQq3X-bSwDMBbegusB0P3lmv9"
+access_token = "ya29.a0AfH6SMBnbZt4Y4mYKjfrtWYVl14YUigQH8H3fQogCBlrbdX_VA5hUgzMWrv1A15dN9GnraG-lPt3NCRO_R1NowysUR3F2-SDijyxkTLt84RudJH-7HhKwjkinWjlhEGsX3TEnHE8PqE9KsemB7u_aBo1-l5O"
 
 headers = {
-  "Authorization": "Bearer {}".format(access_token),
-  "Content-Type": "application/json;encoding=utf-8"
-  }
+    "Authorization": "Bearer {}".format(access_token),
+    "Content-Type": "application/json;encoding=utf-8"
+}
 
 response = requests.get(api_url, headers=headers)
 
+json_response = response.json()
 
+point = json_response["point"]
 
-print(response.text)
+ctr = 0
+
+with open('weights.csv', mode='w', newline='') as csv_file:
+	fieldnames = ['Date', 'Weight', 'BMI', 'Fat']
+	writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+	writer.writeheader()
+
+	for i in range(len(point)):
+		weight = float(round(point[i]["value"][0]["fpVal"], 2))
+		time_in_nanos = float(point[i]["startTimeNanos"])
+		time_in_secs = time_in_nanos / 1e9
+		dt = datetime.datetime.fromtimestamp(time_in_secs).strftime('%Y-%m-%d')
+		writer.writerow({'Date': dt, 'Weight': weight, 'BMI': '0', 'Fat': '0'})
+		ctr += 1	
+
+print(f"There are {ctr} weigh-ins")
